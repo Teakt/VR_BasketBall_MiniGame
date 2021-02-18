98a4ebaf6145428ac2e87550a73f52e673b76d44
Teak <teakguillaume@gmail.com>
2021-02-18 16:24:04 +0100

    Added the score on the canvas / Detect a basket correctly /

---
+++ Assets/BasketChecker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketChecker : MonoBehaviour
+{
+
+
+    [SerializeField] private ScoreManager score_manager;
+
+    protected  void Awake()
+    {
+        
+        
+
+      
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider ball)
+    {
+        if (ball.gameObject.CompareTag("Ball"))
+        {
+            score_manager.SetScore(score_manager.GetScore() + 1);
+        }
+    }
+
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    [Header("[Score Management]")]
+    [SerializeField] private Text score_text;
+    [SerializeField] private GameObject basket_checker; // object placed in basket checking if a ball marked or not 
+
+    public int score_Player { get; set; } // the score is saved and modified here
+   
+    /*-----------------------------------------------------------------------------*/
+    public delegate void OnScoreChangeEvent(int score);
+    public event OnScoreChangeEvent OnScoreChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score_Player = 0;
+        score_text.text = "" + score_Player ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score_text.text = "" + score_Player;
+    }
+
+
+    public int GetScore() //  et de même pour OnScoreChange avec le score. 
+    {
+        return score_Player;
+    }
+
+    public void SetScore(int score) //  et de même pour OnScoreChange avec le score. 
+    {
+        score_Player= score;
+    }
+}
